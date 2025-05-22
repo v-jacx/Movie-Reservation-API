6ddd62c1ca66089e4ec08b241512c56591619b88
v-jacx <jacquelinv92@gmail.com>
2025-05-22 13:22:13 -0500

    generated models without relationships

---
+++ models/seat.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Seat extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Seat.init({
+    number: DataTypes.CHAR(100)
+  }, {
+    sequelize,
+    modelName: 'Seat',
+    tableName: 'seats'
+  });
+  return Seat;
+};
